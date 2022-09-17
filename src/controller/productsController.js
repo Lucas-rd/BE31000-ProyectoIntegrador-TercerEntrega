5734d22dc448d0c26d60b6c4835ba9ba46784958
ltrodrig <lrodrdov@emeal.nttdata.com>
2022-09-17 18:06:01 -0300

    first commit

---
+++ src/controller/productsController.js
@@ -0,0 +1,49 @@
+import { productDAO } from "../DAO/productDAO.js"
+import logger from "../utils/logger.js"
+
+const getAllProductsController = async (req, res) => {
+    try {
+        // const { user } = req.session
+        //Este req.user viene de la estrategia definida de passport.local y ya no hace falta recurrir al req.session
+        const { email } = req.user
+        //en este controller debo capturar el id de carrito del user para usarlo en el llamado a los metodos de cart.
+        console.log(req.user.username)
+        console.log(req.user._id)
+        const products = await productDAO.getAll()
+    
+        res.render("plantillaProducts.ejs", { email, products })
+    } catch (error) {
+        logger.error(error)
+    }
+}
+
+const getOneProductController = async (req, res) => {
+    try {
+        const { id } = req.params
+        const { email } = req.user
+    
+        const products = await productDAO.getById(id)
+    
+        res.render("plantillaProducts.ejs", { email, products })
+    } catch (error) {
+        logger.error(error)
+        res.redirect('/api/products/all')
+    }
+}
+
+const postNewProduct = async (req, res) => {
+    try {
+        const newProduct = req.body
+        const { email } = req.user
+    
+        await productDAO.createDocument(newProduct)
+        const products = await productDAO.getAll()
+        
+        res.render("plantillaProducts.ejs", { email, products })
+    } catch (error) {
+        logger.error(error)
+        res.redirect('/api/products/all')
+    }
+}
+
+export { getAllProductsController, getOneProductController, postNewProduct }
